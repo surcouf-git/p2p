d593d1fb58d4516ea858a2cce69f818215bae688
Your Name <you@example.com>
2025-12-24 19:07:57 +0100

    refactor everything; working on saving remote offers

--- p2p.js
+++ p2p.js
@@ -2,70 +2,142 @@
 // 24/12/25
 //
 
+// ===== global ===== //
 let peerConnexion = null;
+//===================//
+
+// ============================ remote save ============================= //
+let remoteOffer = null;
+let remoteCandidates = [];
+let remoteFullOffer = { remoteOffer, remoteCandidates };
+
+function signalError(status) {
+	let message = (status === "set" ? "CETTE BOX NE PEUT ETRE VIDE !" : "");
+	document.getElementById("onError").innerHTML = message;
+}
+
+async function saveRemoteOffer() {
+	await peerConnexion.setRemoteDescription(remoteOffer);
+}
+
+async function saveRemoteCandidates() {
+	for (let i in remoteFullOffer.remoteCandidates.size()) {
+		await peerConnexion.addIceCandidate(remoteFullOffer.remoteCandidates[i]);
+		console.log('done saving candidate:', remoteFullOffer.remoteCandidates[i]);
+	}
+}
+
+async function saveRemoteFullOffer() {
+	remoteFullOffer = JSON.parse(document.getElementById("peerOfferTxtArea"));
+	if (!remoteFullOffer) {
+		signalError("set");
+		console.log("box empty");
+		return ;
+	}
+	
+	console.log("box not empty");
+
+	saveRemoteOffer();
+
+	saveRemoteCandidates();
+}
+
+document.getElementById("saveButton").addEventListener('click', async function applyRemoteOffer(event) {
+	//event.preventDefault();
+
+	//await peerConnexion.setRemoteDescription(JSON.parse(remoteOffer.value));
+	//const answer = await peerConnexion.createAnswer();
+	//await peerConnexion.setLocalDescription(answer);
+	//console.log("Answer: ", answer);
+	//const giveAnswer = document.getElementById("answerTextArea");
+	//giveAnswer.value = JSON.stringify(answer);
+	//displayDiv("submitAnswer", "block");
+});
+
+// ============================ local generate ============================= //
+
+let offer = null;
+let candidates = [];
+let fullOffer = { offer, candidates };
+
+function copyTxtButton() {
+	var copyText = document.getElementById("fullOfferTxtArea");
+	copyText.select();
+	copyText.setSelectionRange(0, 99999)
+	document.execCommand("copy");
+	document.getElementById("copyOffer").innerHTML = "COPIÉ !"
+}
 
 function switchPage() {
 	document.getElementById('page1').style.display = 'none';
 	document.getElementById('page2').style.display = 'block';
 }
 
-function displayPage(name, status) {
+function displayDiv(name, status) {
 	document.getElementById(name).style.display = status;
 }
 
-document.getElementById("generateOffer").addEventListener('click', async function generateOffer(event) {
-	let offer;
-
-	if (!peerConnexion.localDescription) {
-		offer = await peerConnexion.createOffer();
-		console.log("Very new offer generated:", offer);
-		await peerConnexion.setLocalDescription(offer);
-		displayPage("generateOfferTextArea", "block");
-	} else {
-		offer = peerConnexion.localDescription;
-		console.log("Doesnt need a new offer");
-	}
-	const textArea = document.getElementById("generateOfferTextArea");
-	textArea.value = await JSON.stringify(offer);
-});
-
-document.getElementById("submitOffer").addEventListener('click', async function applyRemoteOffer(event) {
-	let remoteOffer;
-	
-	remoteOffer = document.getElementById("submitOfferTextArea");
-	if (!remoteOffer) {
-		//signal this is wrong
-	}
-	await peerConnexion.setRemoteDescription(JSON.parse(remoteOffer.value));
-	const answer = await peerConnexion.createAnswer();
-	await peerConnexion.setLocalDescription(answer);
-	console.log("Answer: ", answer);
-	const giveAnswer = document.getElementById("answerTextArea");
-	giveAnswer.value = JSON.stringify(answer);
-	displayPage("submitAnswer", "block");
-});
-
-async function createMainObj(server) {
-	peerConnexion = new RTCPeerConnection({ iceServers: [{ urls: server }] });
-
-	//peerConnexion.onicecandidate = (event) => {
-	//	if (event.candidate) {
-	//		console.log("✅ Serveur STUN fonctionne !");
-	//		console.log("Candidat reçu:", event.candidate.candidate);
-	//	} else {
-	//		console.log("Failed to connect");
-	//	}
-	//};
-
-	console.log('connexion state:',peerConnexion.connectionState, " -- offer: ", peerConnexion.offer);
+async function displayTextToDiv(name, text) {
+	document.getElementById(name).value = JSON.stringify(text);
 }
 
-// Automatic STUN
+async function displayFullOfferToClient() {
+	displayDiv("fullOfferTxtArea", "block");
+	await displayTextToDiv("fullOfferTxtArea", fullOffer);
+}
+
+async function generateOffer() {
+	fullOffer.offer = await peerConnexion.createOffer();
+	console.log("Offer created");
+};
+
+async function saveLocalOffer() {
+	await peerConnexion.setLocalDescription(offer);
+	console.log("local description set");
+}
+
+function createObj(server) {
+	console.log("create main obj called!");
+	
+	peerConnexion = new RTCPeerConnection({ iceServers: [{ urls: server }] });
+}
+
+function createChannel() {
+	peerConnexion.createDataChannel("OGchan");
+}
+
+function collectCandidates() {
+	peerConnexion.onicecandidate = (event) => {
+		if (event.candidate) {
+			console.log("new candidate: ", event.candidate.candidate);
+			fullOffer.candidates.push(event.candidate);
+		} else {
+			displayFullOfferToClient();
+			console.log("all candidates found");
+		}
+	};
+}
+
+async function initPeerConnection(server) {
+
+	createObj(server);
+
+	createChannel();
+
+	await generateOffer();
+	await saveLocalOffer();
+
+	collectCandidates();
+
+	console.log("init done, waiting for candidates");
+}
+
+// Automatic STUN -- init all stuff 
 document.getElementById('autoIP').addEventListener('click', async function autoSTUN(event) {
 	event.preventDefault();
-	console.log("Automatique ! ");
 	switchPage();
-	await createMainObj("stun:stun.services.mozilla.com:3478");
+	//await initPeerConnection("stun:stun.services.mozilla.com:3478");
+	await initPeerConnection("stun:stun.l.google.com:19302");
 	//stun:stun.l.google.com:19302
 	// add more
 });
